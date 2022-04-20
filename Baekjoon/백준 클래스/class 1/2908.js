8518d98d4751625fd526082c1a5bccde92f19ef0
[SW2/서울/1반] 박재현 <jaehyeonp74@gmail.com>
2022-04-20 20:49:28 +0900

    1152 2908

---
+++ Baekjoon/백준 클래스/class 1/2908.js
@@ -0,0 +1,6 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(" ");
+
+const [a, b] = input.map(v => [...v].reverse().join(''));
+
+console.log(a > b ? a : b);
+// console.log(Math.max(a, b));
