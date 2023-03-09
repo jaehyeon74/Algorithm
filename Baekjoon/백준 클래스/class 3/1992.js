dcc39ba24cd2193688b83a201aa7529db9e3474f
Jaehyeon <jaehyeonp74@gmail.com>
2023-03-09 15:16:01 +0900

    1992 보충설명

--- Baekjoon/백준 클래스/class 3/1992.js
+++ Baekjoon/백준 클래스/class 3/1992.js
@@ -31,3 +31,8 @@ function recursion(n, x, y) {
 
 recursion(N, 0, 0);
 console.log(quadTree.join(""));
+
+// 색종이 만들기 문제와 비슷
+// recursion에서 현재 화면이 모두 같은 색이면 현재 색깔을 quadTree에 추가하고,
+// 그렇지 않으면 화면을 4분할하여 각각의 화면별로 재귀호출한다
+// 괄호는 화면이 분할되기 전과 후에만 필요
