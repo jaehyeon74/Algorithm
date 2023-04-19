4e391e1e4397e47fd7e42e875b5b4b3083cd0a9c
Jaehyeon <jaehyeonp74@gmail.com>
2023-04-19 17:00:08 +0900

    자료구조 스택을 직접 구현해 푼 dfs 풀이

--- Baekjoon/백준 클래스/class 3/11724.js
+++ Baekjoon/백준 클래스/class 3/11724.js
@@ -1,34 +1,90 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
-const [N, M] = input.shift().split(" ").map(Number);
 
-const edges = input.map((v) => v.split(" ").map(Number));
+const [N, M] = input.shift().split(" ").map(Number);
+const edges = input.map((elem) => elem.split(" ").map(Number));
 const graph = Array.from({ length: N + 1 }, () => []);
 edges.forEach(([from, to]) => {
   graph[from].push(to);
   graph[to].push(from);
 });
-const visited = Array(N + 1).fill(false);
+let cnt = 0;
 
-const dfs = (start) => {
-  const stack = [start];
-  const order = [];
-  while (stack.length) {
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  push(...values) {
+    values.forEach((value) => {
+      const node = new Node(value);
+      if (!this.first) {
+        this.first = node;
+        this.last = node;
+      } else {
+        let temp = this.first;
+        this.first = node;
+        this.first.next = temp;
+      }
+      this.size++;
+    });
+  }
+
+  pop() {
+    if (!this.first) return null;
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.first = this.last = null;
+    } else {
+      this.first = this.first.next;
+    }
+    this.size--;
+    return temp.value;
+  }
+
+  clear() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+}
+
+function solve(N, graph) {
+  let count = 0;
+  const visited = new Array(N + 1).fill(false);
+  const stack = new Stack();
+  for (let i = 1; i <= N; i++) {
+    if (!visited[i]) {
+      dfs(i, visited, stack, graph);
+      count++;
+    }
+  }
+  return count;
+}
+
+function dfs(start, visited, stack, graph) {
+  if (graph[start].length === 0) {
+    visited[start] = true;
+    return;
+  }
+  stack.clear();
+  stack.push(start);
+  while (stack.size) {
     const node = stack.pop();
     if (!visited[node]) {
       visited[node] = true;
       stack.push(...graph[node]);
-      order.push(node);
     }
   }
-  return order.sort((a, b) => a - b).join(" ");
-};
-
-let count = 0;
-for (let i = 1; i <= N; i++) {
-  const str = dfs(i);
-  if (str.length) count++;
 }
 
-console.log(count);
+console.log(solve(N, graph));
