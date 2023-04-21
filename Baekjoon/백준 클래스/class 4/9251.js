3590f0935cbb17cd0c9a1df8f1b43756518f6572
Jaehyeon <jaehyeonp74@gmail.com>
2023-04-21 15:34:52 +0900

    9251 LCS 다른 풀이

--- Baekjoon/백준 클래스/class 4/9251.js
+++ Baekjoon/백준 클래스/class 4/9251.js
@@ -10,24 +10,50 @@ let input = fs
 
 const [S1, S2] = input;
 
-function LCS(string1, string2) {
-  const lcs = (s1, s2, memo = {}) => {
-    if (!s1 || !s2) return "";
-    if (memo[`${s1}:${s2}`]) return memo[`${s1}:${s2}`];
-    if (s1[0] === s2[0])
-      return s1[0] + lcs(s1.substring(1), s2.substring(1), memo);
+function LongestCommonSubsequence(s1, s2) {
+  const lcsMatrix = Array.from({ length: s2.length + 1 }, () =>
+    Array(s1.length + 1).fill(null)
+  );
+  for (let columnIndex = 0; columnIndex <= s1.length; columnIndex++) {
+    lcsMatrix[0][columnIndex] = 0;
+  }
+  for (let rowIndex = 0; rowIndex <= s2.length; rowIndex++) {
+    lcsMatrix[rowIndex][0] = 0;
+  }
+  for (let rowIndex = 1; rowIndex <= s2.length; rowIndex++) {
+    for (let columnIndex = 1; columnIndex <= s1.length; columnIndex++) {
+      if (s1[columnIndex - 1] === s2[rowIndex - 1]) {
+        lcsMatrix[rowIndex][columnIndex] =
+          lcsMatrix[rowIndex - 1][columnIndex - 1] + 1;
+      } else {
+        lcsMatrix[rowIndex][columnIndex] = Math.max(
+          lcsMatrix[rowIndex - 1][columnIndex],
+          lcsMatrix[rowIndex][columnIndex - 1]
+        );
+      }
+    }
+  }
 
-    const nextLCS1 = lcs(s1.substring(1), s2, memo);
-    const nextLCS2 = lcs(s1, s2.substring(1), memo);
-    const nextLongest =
-      nextLCS1.length >= nextLCS2.length ? nextLCS1 : nextLCS2;
+  if (!lcsMatrix[s2.length][s1.length]) return "";
+  const longestSequence = [];
+  let columnIndex = s1.length;
+  let rowIndex = s2.length;
 
-    memo[`${s1}:${s2}`] = nextLongest;
+  while (columnIndex > 0 || rowIndex > 0) {
+    if (s1[columnIndex - 1] === s2[rowIndex - 1]) {
+      longestSequence.unshift(s1[columnIndex - 1]);
+      columnIndex -= 1;
+      rowIndex -= 1;
+    } else if (
+      lcsMatrix[rowIndex][columnIndex] === lcsMatrix[rowIndex][columnIndex - 1]
+    ) {
+      columnIndex -= 1;
+    } else {
+      rowIndex -= 1;
+    }
+  }
 
-    return nextLongest;
-  };
-
-  return lcs(string1, string2);
+  return longestSequence.join("");
 }
 
-console.log(LCS(S1, S2));
+console.log(LongestCommonSubsequence(S1, S2));
