bed7bf422775bbf46673e1db37fdb558d9c4eacf
Jaehyeon <jaehyeonp74@gmail.com>
2023-05-23 15:08:13 +0900

    11053 풀이 보충

--- Baekjoon/dp/11053.js
+++ Baekjoon/dp/11053.js
@@ -6,11 +6,13 @@ const N = +input[0];
 const arr = input[1].split(" ").map(Number);
 
 const dp = Array(N).fill(0);
-let max = 0;
 for (let i = 0; i < N; i++) {
-  max = 0;
+  let max = 0;
   for (let j = 0; j < i; j++) {
     if (arr[i] > arr[j] && dp[j] > max) {
+      // 여기서 만약 dp[j]>max 조건이 없다면
+      // 최대값이 max값으로 대입되는 것이 아니라 가장 마지막에 들어온 dp[j]
+      // 값이 max 값으로 설정된다
       max = dp[j];
     }
   }
