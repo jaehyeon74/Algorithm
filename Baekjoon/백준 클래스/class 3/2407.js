3ad62b979fc51ceb533bdeaa5e458d2cf47fc09f
Jaehyeon <jaehyeonp74@gmail.com>
2023-06-03 13:41:11 +0900

    조합 다른 풀이

--- Baekjoon/백준 클래스/class 3/2407.js
+++ Baekjoon/백준 클래스/class 3/2407.js
@@ -1,20 +1,14 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
-let input = fs.readFileSync(filePath).toString().trim();
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const [n, m] = input.split(" ").map(Number);
+const [N, M] = input[0].split(" ").map(BigInt);
+let answer = 1n;
 
-const arr = Array.from({ length: n + 1 }, () => Array(m + 1).fill(0));
-
-for (let i = 0; i <= n; i++) {
-  arr[i][0] = 1;
-  arr[i][i] = 1;
+for (let i = 0; i < Number(M); i++) {
+  answer *= N - BigInt(i);
+  answer /= BigInt(i) + 1n;
 }
 
-for (let i = 1; i <= n; i++) {
-  for (let j = 1; j <= m; j++) {
-    arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
-  }
-}
+console.log(answer.toString());
 
-console.log(arr[n][m]);
