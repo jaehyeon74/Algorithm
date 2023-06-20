de59c385b9363ee9e5cb05c7c83ca9aa9aedec0e
Jaehyeon <jaehyeonp74@gmail.com>
2023-06-20 21:17:58 +0900

    그리디 택배 배달과 수거하기

--- Programmers/LV3/택배 배달과 수거하기.js
+++ Programmers/LV3/택배 배달과 수거하기.js
@@ -1,55 +1,50 @@
 function solution(cap, n, deliveries, pickups) {
-  let dist = 0;
-  const boxMap = new Map();
-  for (let i = n - 1; i >= 0; i--) {
-    const delivery = deliveries[i];
-    const pickup = pickups[i];
-    if (delivery > 0 || pickup > 0) boxMap.set(i + 1, [delivery, pickup]);
-  }
+  let answer = 0;
+  let deliveriesIndex = n - 1;
+  let pickupsIndex = n - 1;
 
-  while (boxMap.size) {
-    let deliverAmount = 0;
-    let pickupAmount = 0;
+  while (deliveriesIndex >= 0 || pickupsIndex >= 0) {
+    let deliver_cap = 0;
+    let pickup_cap = 0;
 
-    let maxDeliveryD = 0;
-    let maxPickupD = 0;
+    while (deliveries[deliveriesIndex] == 0) {
+      deliveriesIndex--;
+    }
 
-    for (let [key, [beforeDelivery, beforePickUp]] of boxMap) {
-      let updateDeliveryAmount = beforeDelivery;
-      let updatePickupAmount = beforePickUp;
+    while (pickups[pickupsIndex] == 0) {
+      pickupsIndex--;
+    }
 
-      if (deliverAmount == cap && pickupAmount == cap) break;
-      if (beforeDelivery !== 0 && deliverAmount + beforeDelivery <= cap) {
-        deliverAmount += beforeDelivery;
-        updateDeliveryAmount = 0;
-        maxDeliveryD = Math.max(maxDeliveryD, key);
-      } else if (beforeDelivery !== 0 && deliverAmount + beforeDelivery > cap) {
-        updateDeliveryAmount = beforeDelivery + deliverAmount - cap;
-        deliverAmount = cap;
-        maxDeliveryD = Math.max(maxDeliveryD, key);
-      }
+    answer += Math.max(deliveriesIndex + 1, pickupsIndex + 1) * 2;
 
-      if (beforePickUp !== 0 && pickupAmount + beforePickUp <= cap) {
-        pickupAmount += beforePickUp;
-        updatePickupAmount = 0;
-        maxPickupD = Math.max(maxPickupD, key);
-      } else if (beforePickUp !== 0 && pickupAmount + beforePickUp > cap) {
-        updatePickupAmount = beforePickUp + pickupAmount - cap;
-        pickupAmount = cap;
-        maxPickupD = Math.max(maxPickupD, key);
-      }
-
-      if (updateDeliveryAmount == 0 && updatePickupAmount == 0) {
-        boxMap.delete(key);
+    // do best to delivery
+    for (let i = deliveriesIndex; i >= 0; i--) {
+      if (deliver_cap + deliveries[i] <= cap) {
+        deliver_cap += deliveries[i];
+        deliveries[i] = 0;
+        deliveriesIndex--;
       } else {
-        boxMap.set(key, [updateDeliveryAmount, updatePickupAmount]);
+        deliveries[i] = deliver_cap + deliveries[i] - cap;
+        deliveriesIndex = i;
+        break;
       }
     }
 
-    dist += Math.max(maxDeliveryD, maxPickupD);
+    // do best to pickup
+    for (let i = pickupsIndex; i >= 0; i--) {
+      if (pickup_cap + pickups[i] <= cap) {
+        pickup_cap += pickups[i];
+        pickups[i] = 0;
+        pickupsIndex--;
+      } else {
+        pickups[i] = pickup_cap + pickups[i] - cap;
+        pickupsIndex = i;
+        break;
+      }
+    }
   }
 
-  return dist * 2;
+  return answer;
 }
 
 console.log(solution(4, 5, [1, 0, 3, 1, 2], [0, 3, 0, 4, 0]));
